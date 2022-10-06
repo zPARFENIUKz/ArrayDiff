a757802189836880710585b10505852f0e40696c
Gregory <parfeniuk33@gmail.com>
2022-10-06 22:35:46 +0300

    My implementation

--- src/ArrayDiff.java
+++ src/ArrayDiff.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class ArrayDiff {
+import java.util.Arrays;
+
+public class ArrayDiff {
+    public static void main(String[] args){
+        final int[] res = arrayDiff(new int [] {1,2,2}, new int[] {1});
+        System.out.println(Arrays.toString(res));
+    }
+
+    public static int[] arrayDiff(final int[] a, final int[] b) throws NullPointerException{
+        if (a == null || b == null) throw new NullPointerException("arrays cannot be null pointers");
+        return Arrays.stream(a).filter(el -> Arrays.stream(b).noneMatch(e -> e == el)).toArray();
+    }
 }
